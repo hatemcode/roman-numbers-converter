5c5dc3da597c36239c45441c1f907eb4f1b56adb
Hatem A <hatemcode@users.noreply.github.com>
2016-08-29 16:12:47 +0400

    Add description

--- js/roman-numbers-converter.js
+++ js/roman-numbers-converter.js
@@ -1,3 +1,10 @@
+/**
+ * @Roman Numbers Converter
+ * @author Hatem A. <hatem@tuta.io>
+ * @version 1.0
+ * @licence Licensed under the Apache License, Version 2.0
+ */
+ 
 if(typeof roman == "undefined"){
   var roman = (function(){
 
