dd68fd1cc8a6de0915aeab9da462dd5aeb28a4c4
Hatem A <hatemcode@users.noreply.github.com>
2016-08-30 22:56:11 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@
  */
  
 $(document).ready(function(){
-  
-  // on click convert
+    
+  // on click convert to roman
   $("#convert-to-roman-button").click(function(event1){
     event1.preventDefault();
     
@@ -27,6 +27,31 @@ $(document).ready(function(){
       
     // if not show error  
     }else{
+      $("#error").html("Please write Integer only (except 0) !");
+      $("#error").show();
+    }
+  });
+
+    // on click convert to number
+  $("#convert-to-number-button").click(function(event1){
+    event1.preventDefault();
+    
+    // hide previous errors and results
+    $(".error").hide();
+    $(".result").hide();
+    
+    // get value and convert it to integer
+    var value = $("#roman-number-input").val();
+    var result = roman.convertToNumber(value);
+    
+    // if convertion is ok show the result
+    if(result !== null && value != ""){
+      $("#number-result").html(result);
+      $("#number-result").show();
+      
+    // if not show error  
+    }else{
+      $("#error").html("Please write Roman Number !");
       $("#error").show();
     }
   });
@@ -37,9 +62,21 @@ $(document).ready(function(){
     var value = $("#regular-number-input").val();
     var index = value.length - 1;
     
-    if(!value[index].match(/\d/g)){
+    if(!value[index].match(/[1-9]+/g)){
       value = value.substring(0,index);     
       $("#regular-number-input").val(value);
+    }
+  });
+
+  $("#roman-number-input").keyup(function(){
+    
+    var value = $("#roman-number-input").val();
+    $("#roman-number-input").val(value.toUpperCase());
+    var index = value.length - 1;
+    
+    if(!value[index].match(/[IiVvMmLlXxMmCcDd]/g)){
+      value = value.substring(0,index);     
+      $("#roman-number-input").val(value.toUpperCase());
     }
   });
 });
